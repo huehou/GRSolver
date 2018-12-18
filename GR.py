77ccf42cea9a237f32e44322b8ffc002b7b9242a
huehou <Alexander_hue1993@hotmail.com>
2018-12-18 16:30:45 +0800

    Coded Christoffel symbols

--- GR.py
+++ GR.py
@@ -20,7 +20,17 @@ class GR():
         self.Christoffel = None
 
     def Christ(self):
-        pass 
+        Christoffel = np.ones([self.dim, self.dim, self.dim])*coord[0]
+        for i in range(self.dim):
+            for j in range(self.dim):
+                for k in range(self.dim):
+                    res = 0
+                    for s in range(self.dim):
+                        res += self.inversemetric[i,s]*(sym.diff(self.metric[s,j], self.coord[k]) + sym.diff(self.metric[s,k], self.coord[j]) - sym.diff(self.metric[j,k], self.coord[s]))
+                    res /= 2
+                    Christoffel[i,j,k] = res
+        self.Christoffel = Christoffel
+        return Christoffel
 
 if __name__ == "__main__":
     t, r, theta, phi = sym.symbols('t r theta phi')
@@ -29,5 +39,4 @@ if __name__ == "__main__":
     metric = sym.Matrix([[-1 + 2*m/r, 0, 0, 0], [0, (1 - 2*m/r)**(-1), 0, 0], [0, 0, r**2, 0], [0, 0, 0, r**2 * sym.sin(theta)**2]])
     test = GR(coord, metric)
     print(test.metric)
-    print(test.inversemetric)
-    print(test.dim)
+    print(test.Christ())
