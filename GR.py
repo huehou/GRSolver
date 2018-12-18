6a4a1750d2b8780db0c450cd216be8c4dc9f3d44
huehou <Alexander_hue1993@hotmail.com>
2018-12-18 17:12:40 +0800

    Coded Ricci tensor and display option

--- GR.py
+++ GR.py
@@ -19,6 +19,7 @@ class GR():
         self.dim = len(coord)
         self.Christoffel = None
         self.Riemann = None
+        self.Ricci = None
 
     def Christ(self, display = False):
         '''
@@ -69,10 +70,28 @@ class GR():
                                 print('Riemann[{},{},{},{}] = {}'.format(i,j,k,l,Riemann[i,j,k,l]))
         return self.Riemann
 
+    def Ric(self, display = False):
+        if type(self.Ricci) == type(None):
+            Ricci = np.ones([self.dim, self.dim])*coord[0]
+            for j in range(self.dim):
+                for l in range(self.dim):
+                    res = 0
+                    for i in range(self.dim):
+                        res += self.Riem()[i,j,i,l]
+                    Ricci[j,l] = sym.simplify(res)
+            self.Ricci = Ricci
+        if display:
+            Ricci = self.Ricci
+            for j in range(self.dim):
+                for l in range(j+1):
+                    if Ricci[j,l] != 0:
+                        print('Ricci[{},{}] = {}'.format(j, l, Ricci[j,l]))
+        return self.Ricci
+
 if __name__ == "__main__":
     t, r, theta, phi = sym.symbols('t r theta phi')
     coord = [t, r, theta, phi]
     m = sym.symbols('m')
     metric = sym.Matrix([[-1 + 2*m/r, 0, 0, 0], [0, (1 - 2*m/r)**(-1), 0, 0], [0, 0, r**2, 0], [0, 0, 0, r**2 * sym.sin(theta)**2]])
     test = GR(coord, metric)
-    test.Riem(True)
+    test.Ric(True)
